f0c78bdd0675418bb2601ff6cea5f6b74101dd80
One4Seven_ <212090123@stu.nau.edu.cn>
2024-09-24 17:35:55 +0800

    1

--- JavaSE/experimental-case-app/src/com/yst/ATM_system/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.yst.ATM_system;
-
-public class Demo {
-    public static void main(String[] args) {
-        // 模拟银行开户和一些基本业务
-        ATM atm = new ATM();
-        atm.Start();
-    }
-}
